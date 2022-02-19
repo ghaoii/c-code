697c9c8ef945d67a157c5ff12776b8b7c9faf71c
ghaoii <641517196@qq.com>
2022-02-19 22:46:32 +0800

    代码提交

--- 函数/函数/test_c.cpp
+++ 函数/函数/test_c.cpp
@@ -2,3 +2,40 @@
 
 #include <stdio.h>
 
+#include <string.h>
+//int main()
+//{
+//	char arr1[] = "bit";
+//	char arr2[20] = "########";
+//	strcpy(arr2, arr1);//string copy-字符串拷贝 其实是arr1里的字符串和arr2里的相对的下标的内容替换了
+//	printf("%s\n", arr2);//由于把\0也拷贝了，因此字符串是"bit\0",arr2后面的 # 无法正常打印
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "hello world";
+//	memset(arr, '*', 5);//将arr中的前5个字节的内容替换成*
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//比较两个数的大小
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	printf("%d\n", max);
+//	max = get_max(100, 300);
+//	printf("%d\n", max);
+//	return 0;
+//}
+
