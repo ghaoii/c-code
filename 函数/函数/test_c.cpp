5f59f7eae0f4a63f686573e4f8f29954952629ec
ghaoii <641517196@qq.com>
2022-02-20 19:09:37 +0800

    代码提交

--- 函数/函数/test_c.cpp
+++ 函数/函数/test_c.cpp
@@ -39,3 +39,179 @@
 //	return 0;
 //}
 
+// 交换两个变量的内容
+//void Swap1(int x, int y)//不能完成任务
+//{
+//	int tmp = 0;
+//	tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+////当实参传给形参的时候
+////形参其实是实参的ii一份临时拷贝
+////对形参的修改是不会改变实参的
+//void Swap2(int* pa, int* pb)
+//{
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a = %d, b = %d\n", a, b);
+//	//函数调用
+//	//Swap1(a, b);//传值调用
+//	Swap2(&a, &b);//传址调用
+//	printf("a = %d, b = %d\n", a, b);
+//	return 0;
+//}
+
+//#include <math.h>
+////是素数返回1，不是素数返回0
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j <= sqrt(n); j++)
+//	{
+//		if (n % j == 0)
+//			return 0;//return的操作程度要大于break，return直接退出函数，break只是跳出循环
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	//打印100-200之间的素数
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		is_prime(i);
+//		if (is_prime(i) == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//写一个函数判断一年是不是闰年
+//int is_leep_year(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || (y%400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}//不要在函数里打印是否为闰年，而是只返回值进行判断
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		//判断year是否为闰年
+//		if(1 == is_leep_year(year))
+//			printf("%d ", year);
+//	}
+//		return 0;
+//}
+
+//写一个函数，实现一个整形有序数组的二分查找
+//						//本质上这里的arr是个指针
+//int binary_search(int arr[], int k, int sz)//形参和实参的名字可以相同
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//			left = mid + 1;
+//		else if (arr[mid] > k)
+//			right = mid - 1;
+//		else
+//			return mid;
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	//二分查找
+//	//找到了返回下标，找不到返回-1
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//							//数组传参，传的是数组首元素的地址
+//	int ret = binary_search(arr, k, sz);
+//	if (ret == -1)
+//		printf("找不到\n");
+//	else
+//		printf("找到了，下标是%d\n", ret);
+//	return 0;
+//}
+
+//写一个函数每调用一次这个函数，就会使num的值增加1
+//void Add(int* p)
+//{
+//	(*p)++;//++的优先级要高于*，因此会直接作用在p上
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		Add(&num);
+//	}
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//链式访问
+//int main()
+//{
+//	//1
+//	int len = 0;
+//	len = strlen("abc");
+//	printf("%d\n", len);
+//	//2
+//	printf("%d\n", strlen("abc"));
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));//printf的返回值是打印的字符数量
+//	return 0;
+//}
+
+
+//int Add(int, int);//函数声明
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = 0;
+//	sum = Add(a, b);//函数调用
+//	printf("%d\n", sum);
+//	return 0;
+//}
+////函数定义
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+
+//#include "add.h"
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = 0;
+//	sum = Add(a, b);//函数调用
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
