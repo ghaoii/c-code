1db016f299b728b5f41fd993b557f96605d62729
ghaoii <641517196@qq.com>
2022-02-21 22:31:33 +0800

    代码提交

--- 函数/函数/add.h
+++ 函数/函数/add.h
@@ -1,3 +1,8 @@
 #pragma once
 
+#ifndef __ADD_H__
+#define __ADD_H__
+
 int Add(int x, int y);
+
+#endif;
--- 函数/函数/test_c.cpp
+++ 函数/函数/test_c.cpp
@@ -215,3 +215,67 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	printf("hehe\n");
+//	main();
+//	return 0;
+// }
+
+//接受一个整型值（无符号），按照顺序打印它的每一位
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	//递归
+//	print(num);
+//	return 0;
+//}
+
+//编写函数不允许创建临时变量，求字符串的长度
+
+//递归的方法
+//大事化小
+//my_strlen("bit")
+//1+my_strlen("it")
+//1+1+my_strlen("t")
+//1+1+1+my_strlen("\0")
+//1+1+1=3
+
+int my_strlen(char* str)
+{
+	if (*str != '\0')
+		return 1 + my_strlen(str + 1);
+	else
+		return 0;
+}
+
+//int my_strlen(char* str)//该函数创建了临时变量
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+int main()
+{
+	char arr[] = "bit";
+	//模拟实现strlen函数
+	int len = my_strlen(arr);
+	printf("len = %d", len);
+	return 0;
+}
+
