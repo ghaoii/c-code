20fb0e4ccaa4b1ccdf8e918cfa903b51ba20dabd
ghaoii <641517196@qq.com>
2022-03-08 22:37:02 +0800

    代码提交

--- 初始结构体/初始结构体/test_c.cpp
+++ 初始结构体/初始结构体/test_c.cpp
@@ -43,29 +43,80 @@
 //	return 0;
 //}
 
-struct S
-{
-	int a;
-	char c;
-	char arr[20];
-	double d;
-};
+//struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char* p;
+//};
+//
+//int main()
+//{
+//	char arr[] = "hello bit\n";
+//	struct T t = { "hehe", {10, 'w', "hello world", 3.14}, arr };
+//	printf("%s\n", t.ch);
+//	printf("%s\n", t.s.arr);
+//	printf("%lf\n", t.s.d);
+//	printf("%s\n", t.p);
+//	return 0;
+//}
 
-struct T
-{
-	char ch[10];
-	struct S s;
-	char* p;
-};
 
-int main()
-{
-	char arr[] = "hello bit\n";
-	struct T t = { "hehe", {10, 'w', "hello world", 3.14}, arr };
-	printf("%s\n", t.ch);
-	printf("%s\n", t.s.arr);
-	printf("%lf\n", t.s.d);
-	printf("%s\n", t.p);
-	return 0;
-}
+//结构体传参及成员访问
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//
+//void print1(Stu tmp)
+//{
+//	printf("name = %s\n", tmp.name);
+//	printf("age = %d\n", tmp.age);
+//	printf("tele = %s\n", tmp.tele);
+//	printf("sex = %s\n", tmp.sex);
+//}
+//
+//void print2(Stu* ps)
+//{
+//	printf("name = %s\n", ps->name);
+//	printf("age = %d\n", ps->age);
+//	printf("tele = %s\n", ps->tele);
+//	printf("sex = %s\n", ps->sex);
+//}
+//
+//int main()
+//{
+//	Stu s = { "张三", 30, "15266668888", "男" };
+//	print1(s);//print1传参时，形参会临时拷贝一份数据，如果结构体较大，会压栈，降低性能
+//	print2(&s);//print2更好，因为只会开辟4/8个字节存放地址
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int ret = 0;
+//	ret = Add(a, b);
+//	return 0;
+//}
 
