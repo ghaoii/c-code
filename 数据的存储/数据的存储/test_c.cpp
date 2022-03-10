95b2092b3e8ebd85b18c203387c18c4ddcf07c2d
ghaoii <641517196@qq.com>
2022-03-10 22:45:46 +0800

    代码提交

---
+++ 数据的存储/数据的存储/test_c.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	float f = 10.0;
+//
+//	return 0;
+//}
+
+//void test(void)
+//{
+//	printf("hehe\n");
+//}
+//
+//int main()
+//{
+//	test(100);
+//	return 0;
+//}
+//C语言一开始并没有规定定义函数的时候没设置参数就不能传参
+//如果想要明确表明函数不接收参数的话，可以在括号里写void
+
+//int main()
+//{
+//	int a = 20;
+//	int b = -10;
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 20;
+
+	return 0;
+}
+
